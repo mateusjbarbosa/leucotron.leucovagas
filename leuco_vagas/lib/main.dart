1061df81876d9fc85e5b78983512475a7d105447
Mateus Barbosa <dev.mateusbarbosa@gmail.com>
2020-02-24 20:36:56 -0300

    Criação do tema

--- leuco_vagas/lib/main.dart
+++ leuco_vagas/lib/main.dart
@@ -7,6 +7,11 @@ void main() {
 
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xFF311B92),
+        accentColor: Color(0xFF5641B5),
+      ),
       title: 'Leuco Vagas',
       home: HomeScreen(),
     ),
