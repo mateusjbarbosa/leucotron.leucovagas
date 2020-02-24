b44089d742fdf582bc9c857723485ded0bccc3cd
Mateus Barbosa <dev.mateusbarbosa@gmail.com>
2020-02-24 20:37:11 -0300

    Criação da tela Home

--- leuco_vagas/lib/screens/HomeScreen.dart
+++ leuco_vagas/lib/screens/HomeScreen.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+import './JobScreen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,11 +9,136 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Future _getJobs() async {
+    final firestore = Firestore.instance;
+
+    QuerySnapshot qn = await firestore.collection("jobs").getDocuments();
+
+    return qn.documents;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Leuco Vagas'),
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 250.0,
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
+              ),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(
+                            top: 20.0, left: 20.0, bottom: 10.0),
+                        child: Text(
+                          "Vagas".toUpperCase(),
+                          style: TextStyle(
+                            fontSize: 32.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: FutureBuilder(
+                        future: _getJobs(),
+                        builder: (_, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return Center(
+                              child: Text(
+                                "Buscando vagas...",
+                                style: TextStyle(
+                                  fontSize: 24.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            );
+                          } else {
+                            return ListView.builder(
+                              itemCount: snapshot.data.length,
+                              itemBuilder: (context, i) {
+                                DocumentSnapshot job = snapshot.data[i];
+
+                                return GestureDetector(
+                                  onTap: () => Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (_) => JobScreen(job),
+                                    ),
+                                  ),
+                                  child: Container(
+                                    padding: EdgeInsets.all(20.0),
+                                    decoration: BoxDecoration(
+                                      color: Colors.white60,
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(20.0),
+                                      ),
+                                    ),
+                                    child: Row(
+                                      children: <Widget>[
+                                        Row(
+                                          children: <Widget>[
+                                            CircleAvatar(
+                                              radius: 25.0,
+                                              backgroundColor: Theme.of(context)
+                                                  .primaryColor,
+                                            ),
+                                            SizedBox(
+                                              width: 10.0,
+                                            ),
+                                            Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: <Widget>[
+                                                Text(
+                                                  job.data["name"],
+                                                  style: TextStyle(
+                                                    fontSize: 20.0,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                ),
+                                                Text(
+                                                  job.data["role"],
+                                                  style: TextStyle(
+                                                    fontSize: 14.0,
+                                                    fontWeight: FontWeight.w300,
+                                                  ),
+                                                ),
+                                              ],
+                                            )
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              },
+                            );
+                          }
+                        },
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
