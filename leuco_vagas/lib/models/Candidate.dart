8aef66b1d93882870b697e52ded363f89aed1bc4
Mateus Barbosa <dev.mateusbarbosa@gmail.com>
2020-02-24 17:13:15 -0300

    Criação das classes de tratamento das entidades

---
+++ leuco_vagas/lib/models/Candidate.dart
@@ -0,0 +1,9 @@
+class Candidate {
+  String jobId;
+  String name;
+  Map about;
+  Map experience;
+  List skills;
+
+  Candidate(this.jobId, this.name, this.about, this.experience, this.skills);
+}
---
+++ leuco_vagas/lib/models/Job.dart
@@ -0,0 +1,9 @@
+class Job {
+  String id;
+  String name;
+  String role;
+  List requirements;
+  List skills;
+
+  Job(this.id, this.name, this.role, this.requirements, this.skills);
+}
