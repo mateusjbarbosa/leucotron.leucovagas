bbaedd814718ea09f8f10fa59d11d678c3b749e1
Mateus Barbosa <dev.mateusbarbosa@gmail.com>
2020-02-24 20:39:02 -0300

    Retirada dos modelos

--- leuco_vagas/lib/models/Candidate.dart
+++
@@ -1,9 +0,0 @@
-class Candidate {
-  String jobId;
-  String name;
-  Map about;
-  Map experience;
-  List skills;
-
-  Candidate(this.jobId, this.name, this.about, this.experience, this.skills);
-}
--- leuco_vagas/lib/models/Job.dart
+++
@@ -1,9 +0,0 @@
-class Job {
-  String id;
-  String name;
-  String role;
-  List requirements;
-  List skills;
-
-  Job(this.id, this.name, this.role, this.requirements, this.skills);
-}
