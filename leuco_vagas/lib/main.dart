bf079e8ee9b97bc5b91310927bf74f619a445c42
Mateus Barbosa <dev.mateusbarbosa@gmail.com>
2020-02-24 17:04:59 -0300

    Criação do projeto e comunicação com o Firebase testada

---
+++ leuco_vagas/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'package:leuco_vagas/screens/HomeScreen.dart';
+
+void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    MaterialApp(
+      title: 'Leuco Vagas',
+      home: HomeScreen(),
+    ),
+  );
+}
