c2de09a0ec51c8f3c05104fc50f68e69c4855914
Mateus Barbosa <dev.mateusbarbosa@gmail.com>
2020-02-25 15:25:03 -0300

    Componentização da seção 'Sobre' do candidato

---
+++ leuco_vagas/lib/widgets/AboutCandidate.dart
@@ -0,0 +1,65 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+
+class AboutCandidate extends StatefulWidget {
+  final LinkedHashMap candidate;
+
+  AboutCandidate(this.candidate);
+
+  @override
+  _AboutCandidateState createState() => _AboutCandidateState();
+}
+
+class _AboutCandidateState extends State<AboutCandidate> {
+  _buildAboutItem(String text) {
+    return Padding(
+      padding: EdgeInsets.only(left: 18.0, top: 10.0, bottom: 5.0),
+      child: Row(
+        children: <Widget>[
+          CircleAvatar(
+            radius: 15.0,
+            backgroundColor: Theme.of(context).primaryColor,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 5.0),
+            child: Text(
+              text,
+              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    LinkedHashMap candidate = widget.candidate;
+
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(left: 18.0, top: 10.0, bottom: 5.0),
+              child: Text(
+                'Sobre ele'.toUpperCase(),
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ],
+        ),
+        _buildAboutItem(candidate['age'] + ' anos'),
+        _buildAboutItem(candidate['course']),
+        _buildAboutItem(candidate['college']),
+        _buildAboutItem(candidate['email']),
+        _buildAboutItem(candidate['cellphone']),
+      ],
+    );
+  }
+}
