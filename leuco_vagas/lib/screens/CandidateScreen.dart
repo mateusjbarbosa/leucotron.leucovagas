74b35ec7d24c917b402bfcb4fac34ada3f226f22
Mateus Barbosa <dev.mateusbarbosa@gmail.com>
2020-02-25 15:24:42 -0300

    Início da criação da tela do candidato

---
+++ leuco_vagas/lib/screens/CandidateScreen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:leuco_vagas/widgets/AboutCandidate.dart';
+
+class CandidateScreen extends StatefulWidget {
+  final DocumentSnapshot candidate;
+
+  CandidateScreen(this.candidate);
+
+  @override
+  _CandidateScreenState createState() => _CandidateScreenState();
+}
+
+class _CandidateScreenState extends State<CandidateScreen> {
+  @override
+  Widget build(BuildContext context) {
+    Map<String, dynamic> candidate = widget.candidate.data;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.candidate.data['name'].toUpperCase(),
+          style: TextStyle(color: Theme.of(context).primaryColor),
+        ),
+        elevation: 0.0,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        iconTheme: IconThemeData(color: Theme.of(context).primaryColor),
+      ),
+      body: Container(
+        child: AboutCandidate(candidate['about']),
+      ),
+    );
+  }
+}
