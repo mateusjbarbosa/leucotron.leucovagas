7fd02f1435fd5ca41f9f0fe5927f81acbe373505
Mateus Barbosa <dev.mateusbarbosa@gmail.com>
2020-02-25 15:23:37 -0300

    Reinicializando

--- leuco_vagas/lib/main.dart
+++ leuco_vagas/lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
+import 'package:leuco_vagas/screens/HomeScreen.dart';
 
+void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Color(0xFF311B92),
         accentColor: Color(0xFF5641B5),
+        scaffoldBackgroundColor: Color(0xFFFAFAFA),
       ),
       title: 'Leuco Vagas',
       home: HomeScreen(),
