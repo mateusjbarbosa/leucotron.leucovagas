bc579a7b5b2585ab2f65dab5cafa5febbdfef531
Mateus Barbosa <dev.mateusbarbosa@gmail.com>
2020-02-25 21:09:40 -0300

    Restart II

--- leuco_vagas/lib/screens/HomeScreen.dart
+++ leuco_vagas/lib/screens/HomeScreen.dart
@@ -1,109 +1,13 @@
 import 'package:flutter/material.dart';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-import 'package:leuco_vagas/screens/JobScreen.dart';
-
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  Future _getJobs() async {
-    final firestore = Firestore.instance;
-
-    QuerySnapshot qn = await firestore.collection("jobs").getDocuments();
-
-    return qn.documents;
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'LEUCO VAGAS',
-          style: TextStyle(color: Theme.of(context).primaryColor),
-        ),
-        elevation: 0.0,
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      ),
-      body: FutureBuilder(
-        future: _getJobs(),
-        builder: (_, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: Text(
-                "Buscando vagas...",
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            );
-          } else {
-            return ListView.builder(
-              itemCount: snapshot.data.length,
-              itemBuilder: (context, i) {
-                DocumentSnapshot job = snapshot.data[i];
-
-                return GestureDetector(
-                  onTap: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => JobScreen(job),
-                    ),
-                  ),
-                  child: Container(
-                    padding: EdgeInsets.all(18.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white60,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(20.0),
-                      ),
-                    ),
-                    child: Row(
-                      children: <Widget>[
-                        Row(
-                          children: <Widget>[
-                            CircleAvatar(
-                              radius: 25.0,
-                              backgroundColor: Theme.of(context).primaryColor,
-                            ),
-                            SizedBox(
-                              width: 10.0,
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text(
-                                  job.data["name"],
-                                  style: TextStyle(
-                                    fontSize: 20.0,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  job.data["role"],
-                                  style: TextStyle(
-                                    fontSize: 14.0,
-                                    fontWeight: FontWeight.w300,
-                                  ),
-                                ),
-                              ],
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                );
-              },
-            );
-          }
-        },
-      ),
-    );
+    return Scaffold();
   }
 }
